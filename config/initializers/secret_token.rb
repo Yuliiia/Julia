b86dd7da76e23437d653a229176ba837c73f842d
Yulia M <liska2aliska@gmail.com>
2014-11-04 10:27:05 +0200

    add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Julia::Application.config.secret_token = '36cf7acfe3641ba8d665451cbe0c14c8515e810df23e54f46573f844d10a603f6dc67067474753ed489e5ac3a37a279d9b9f6231dd3f0936ce3d92cdc227520a'
